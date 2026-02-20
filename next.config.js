fb68a651451f9444b87b32d6a473c390247865e0
Trường Nguyễn <truongnguyen@Truongs-MacBook-Air.local>
2026-02-20 14:49:15 +0700

    remove outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const path = require('path');
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
   output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
